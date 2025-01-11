5f15c8b8437a1c2a47ba7aa59690d3bbb8c6c7de
Izan <izan.valderrabanos@gmail.com>
2025-01-11 21:46:34 +0100

    casos de prueba añadidos

--- practica-promesas/pract1.js
+++ practica-promesas/pract1.js
@@ -6,14 +6,17 @@ const products = {
 
 const checkStock = (productId, quantity) => {
   return new Promise((resolve, reject) => {
-
     //obtenemos el producto del cual queremos consegir la info de name, price o stock. En este caso stock.
     const product = products[productId];
 
     if (product.stock >= quantity) {
-      resolve(`Stock disponible para el producto ${productId}, ${product.name}`);
+      resolve(
+        `Stock disponible para el producto ${productId}, ${product.name}`
+      );
     } else {
-      reject(`Stock insuficiente para el producto ${productId}, ${product.name}`);
+      reject(
+        `Stock insuficiente para el producto ${productId}, ${product.name}`
+      );
     }
   });
 };
@@ -24,7 +27,9 @@ const calculateTotal = (productId, quantity) => {
     const totalPrice = quantity * product.price;
     //no sé aquí muy bien cómo llevar la lógica de la condición
     if (product) {
-      resolve(`Total para ${quantity} unidades de ${product.name}: ${totalPrice}`);
+      resolve(
+        `Precio total para ${quantity} unidades de ${product.name}: ${totalPrice}`
+      );
     } else {
       reject("No se pudo calcular el precio total");
     }
@@ -35,17 +40,22 @@ const confirmOrder = (productId) => {
   return new Promise((resolve, reject) => {
     //el chico del vídeo lo usa para el ejemplo ,pero sería como no poner nada, supongo, si sabemos que se cumplirá siempre
     /* if (true) { */
-      setTimeout(() => {
-        resolve(`Pedido confirmado para el producto ${productId}`);
-      }, 2000);
+    setTimeout(() => {
+      resolve(`Pedido confirmado para el producto ${productId}`);
+    }, 2000);
     /* } */
   });
 };
 
-// Simulación del flujo completo
-const productId = 1;  // Por ejemplo, el producto con ID 1 (Laptop)
-const quantity = 3;    // Queremos comprar 3 unidades
+// Para simulación de flujo, comentar y descomentar
 
+//producto id 1, laptop, comprar 3 unidades
+/* const productId = 1;
+const quantity = 3; */
+
+//producto id 3, keyboard, intentamos comprar número superior a stock
+const productId = 3;
+const quantity = 5;
 
 checkStock(productId, quantity)
   .then((checkMessage) => {
@@ -54,13 +64,13 @@ checkStock(productId, quantity)
     return calculateTotal(productId, quantity);
   })
   .then((totalMessage) => {
-    console.log(totalMessage); 
+    console.log(totalMessage);
     //devuelve el estado de la promesa de confirmación del pedido
-    return confirmOrder(productId);  
+    return confirmOrder(productId);
   })
   .then((confirmationMessage) => {
-    console.log(confirmationMessage); 
+    console.log(confirmationMessage);
   })
   .catch((errorMessage) => {
-    console.log(errorMessage);  
+    console.log(errorMessage);
   });
