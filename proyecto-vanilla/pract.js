3cd929f13c41db16faee28d8b18e742ae67fb8bd
Izan <izan.valderrabanos@gmail.com>
2025-01-15 10:39:03 +0100

    pantalla inicio pruebas

--- proyecto-vanilla/pract.js
+++ proyecto-vanilla/pract.js
@@ -1,59 +1,76 @@
-
-
 /* INICIO */
 /* Pantalla inicial "BIENVENIDO" + pulsar Ctrl + F10 || setTimeOut 5000 */
 
-
+/* al pulsar teclas el usuario, guardar en un array */
 /* gavsblog! */
 let keysPressed = {};
 
-/* al pulsar teclas el usuario, guardar en un array */
-document.addEventListener('keydown', (event) => {
-    /* event.preventDefault(); cuándo hace falta?*/
-    keysPressed[event.key] = true;
-    if(keysPressed['Control'] && event.key == 'F10'){
-        removeWelcome();
-    }
-
-});
-
-/* vaciamos el array, solo queremos su contenido mientras se esté pulsando la tecla */
-document.addEventListener('keyup', (event) => {
-    delete this.keysPressed[event.key];
- });
-
-
 /* después remove con keydown ctrl + f10 o pasados 5 segundos */
-function removeWelcome(){
-    let welcomeDiv = document.getElementById("welcomeMessage");
-    welcomeDiv.remove();
+function removeWelcome() {
+  let welcomeDiv = document.getElementById("welcomeMessage");
+  welcomeDiv.remove();
 }
 
-function createLoginWindow(){
-    /* añadir label */
-    let loginLabel = document.createElement("label");
-    loginLabel.id = "email";
-    let labelMessage = document.createTextNode("Correo electrónico");
-    loginLabel.appendChild(labelMessage);
-    document.body.appendChild(loginLabel); 
-    /* añadir input */
-    let loginInput = document.createElement("input");
-    loginInput.id = "email";
-    loginInput.type = "text";
-    document.body.appendChild(loginInput);
+function createLoginWindow() {
+  /* añadir label */
+  let loginLabel = document.createElement("label");
+  loginLabel.id = "emailLabel";
+  /*enlazamos label e input*/
+  loginLabel.setAttribute("for", "emailInput");
+  let labelMessage = document.createTextNode("Correo electrónico");
+  loginLabel.appendChild(labelMessage);
+  document.body.appendChild(loginLabel);
+
+  /* añadir input */
+  let loginInput = document.createElement("input");
+  loginInput.id = "emailInput";
+  loginInput.type = "text";
+  document.body.appendChild(loginInput);
 }
 
 /* cómo gestionar esto con una promesa, que si se presionan las teclas o temporizador, eliminar, Y luego añadir contenido */
+function startEvent() {
+  return new Promise((resolve) => {
+    /* resolver por temporizador */
+    let timer = setTimeout(() => {
+      resolve("timer");
+    }, 5000);
 
+    /* o bien por interacción usuario */
+    document.addEventListener("keydown", (event) => {
+      /* event.preventDefault(); cuándo hace falta?*/
+      keysPressed[event.key] = true;
+      if (keysPressed["Control"] && event.key === "F10") {
+        clearTimeout(timer);
+        resolve("keys");
+      }
+    });
+
+    /* vaciamos el array, solo queremos su contenido mientras se esté pulsando la tecla */
+    document.addEventListener("keyup", (event) => {
+      delete this.keysPressed[event.key];
+    });
+  });
+}
+
+
+
+startEvent()
+  .then((resolveMessage) => {
+    console.log(`Cambio de pantalla por: ${resolveMessage}`);
+    removeWelcome();
+    createLoginWindow();
+  })
+  .catch((error) => {
+    console.log(`Error: ${error}`);
+  });
 
 
 
 /* LOGIN */
 /*const regexEmail =  caracter/es + @ + caracter/es + . caracter/es */
-let userEmail = document.getElementById('email').value;
+let userEmail = document.getElementById("email").value;
 
-if(test(regexEmail, userEmail)){
-
+if (test(regexEmail, userEmail)) {
 } else {
-
 }
